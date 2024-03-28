49ddc1909434729883be0d4153a32faa8f7efd0e
Ângelo <121000234+Angelo-Andrade@users.noreply.github.com>
2024-03-27 22:09:46 -0300

    Update Livro.java

--- Aula004/pack_ex003_livro2/Livro.java
+++ Aula004/pack_ex003_livro2/Livro.java
@@ -5,7 +5,7 @@ public class Livro {
 	private String autor;
 	private int anoDePublicacao;
 	
-	Livro (String titulo, String autor, int anoDePublicacao) {
+	public Livro (String titulo, String autor, int anoDePublicacao) {
 		this.titulo = titulo;
 		this.autor = autor;
 		this.anoDePublicacao = anoDePublicacao;
