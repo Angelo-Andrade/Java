81b8658628da290d5a729b530946369b826523a6
Ângelo <121000234+Angelo-Andrade@users.noreply.github.com>
2024-08-19 13:49:18 -0300

    Update Main.java

--- Lista 1/Stack/Main.java
+++ Lista 1/Stack/Main.java
@@ -3,7 +3,7 @@ import java.util.Stack;
 public class Main {
     public static void main(String[] args) {
         Stack<String> stack = new Stack<>();
-        for (int i=0; i < 4; i++) stack.push("Element 1");
+        for (int i=0; i < 4; i++) stack.push("Elemento " + i);
         String itemRemoved = stack.pop();
         System.out.println("Popped from stack: " + itemRemoved);
     }
