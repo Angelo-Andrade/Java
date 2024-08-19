887be006edebdbc14c4080a989d71d2ad61d6d6e
Ângelo <121000234+Angelo-Andrade@users.noreply.github.com>
2024-08-19 13:30:20 -0300

    Create Main.java

---
+++ Lista 1/Fila/Main.java
@@ -0,0 +1,14 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class Main {
+    public static void main(String[] args) {
+        Deque<String> queue = new ArrayDeque<>();
+        queue.add("First");
+        queue.add("Second");
+        queue.add("Third");
+
+        String itemRemoved = queue.remove();
+        System.out.println("Removed: " + itemRemoved);
+    }
+}
