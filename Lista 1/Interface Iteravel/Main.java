504e9bc706ae3dbe3185ae88c0c8230603ce66d1
Ângelo <121000234+Angelo-Andrade@users.noreply.github.com>
2024-08-19 19:20:47 -0300

    Create Main.java

---
+++ Lista 1/Interface Iteravel/Main.java
@@ -0,0 +1,12 @@
+package Inteface Iteravel;
+
+public class Main {
+    public static void main(String[] args) {
+        CustomCollection<String> collection = new CustomCollection<>(3);
+        for(int i=0; i<4; i++) collection.add("Item 1");
+
+        for (String item : collection) {
+            System.out.println(item);
+        }
+    }
+}
