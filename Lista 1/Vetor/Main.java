b03b0ee60346d6e4496df65ba1a1dac4467de0ed
Ângelo <121000234+Angelo-Andrade@users.noreply.github.com>
2024-08-19 13:35:19 -0300

    Create Main.java

---
+++ Lista 1/Vetor/Main.java
@@ -0,0 +1,14 @@
+import java.util.Vector;
+
+public class Main {
+    public static void main(String[] args) {
+        Vector<Integer> vector = new Vector<>();
+        for (int i = 0; i < 4; i++) {
+            vector.add(i + 10);
+        }
+
+        for (int i = 0; i < vector.size(); i++) {
+            System.out.println((i + 1) + ": " + vector.get(i));
+        }
+    }
+}
