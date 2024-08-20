6585e794dcff837aa9e7003a4578cf583d6934a8
Ângelo <121000234+Angelo-Andrade@users.noreply.github.com>
2024-08-19 21:02:21 -0300

    Update Main.java

--- Lista 1/InterfaceComparator/Main.java
+++ Lista 1/InterfaceComparator/Main.java
@@ -1,17 +1,26 @@
 package InterfaceComparator;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
 public class Main {
     public static void main(String[] args) {
-        List<Person> people = new ArrayList<>();
-        people.add(new Person("Alice", 30));
-        people.add(new Person("Bob", 25));
-        people.add(new Person("Charlie", 35));
+        List<Produto> produtos = new ArrayList<>();
+        produtos.add(new Produto("Televisão", 1500.0));
+        produtos.add(new Produto("Notebook", 3000.0));
+        produtos.add(new Produto("Celular", 1200.0));
+        produtos.add(new Produto("Tablet", 800.0));
+        produtos.add(new Produto("Mouse", 50.0));
 
-        Collections.sort(people);
-        System.out.println(people);
+        // Ordenando por preço (Comparable)
+        Collections.sort(produtos);
+        System.out.println("Produtos ordenados por preço:");
+        for (Produto produto : produtos) {
+            System.out.println(produto);
+        }
+
+        // Ordenando por nome (Comparator)
+        Collections.sort(produtos, new OrdenarPorNome());
+        System.out.println("\nProdutos ordenados por nome:");
+        for (Produto produto : produtos) {
+            System.out.println(produto);
+        }
     }
 }
